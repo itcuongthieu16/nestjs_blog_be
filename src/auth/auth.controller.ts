08a10133c15e82d99e6b64423bf3ec2882983969
CuongThieu <it.thieutrancuong@gmail.com>
2023-07-04 22:06:00 +0700

    generate access token and refresh token gửi về cho frontend

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { AuthService } from './auth.service';
 import { User } from 'src/user/entities/user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +16,11 @@ export class AuthController {
   @Post('register')
   register(@Body() registerUserDto: RegisterUserDto): Promise<User> {
     return this.authService.register(registerUserDto);
+  }
+
+  @Post('login')
+  @UsePipes(ValidationPipe)
+  login(@Body() loginUserDto: LoginUserDto): Promise<any> {
+    return this.authService.login(loginUserDto);
   }
 }
