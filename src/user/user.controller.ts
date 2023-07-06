cde70a4f5a1d87ace48430e1ba1cd7ada0b38d78
CuongThieu <it.thieutrancuong@gmail.com>
2023-07-06 22:30:17 +0700

    use AuthGuard and xac thuc accesstoken and bearer

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { UserService } from './user.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @UseGuards(AuthGuard)
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+}
